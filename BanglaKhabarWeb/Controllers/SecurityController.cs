fb9a33cfca8ce4264d9b2cb21175df5f7a1fbfc4
Ismith Pasha <ismithpasha@gmail.com>
2019-01-20 01:19:02 +0600

    bk

--- BanglaKhabarWeb/Controllers/SecurityController.cs
+++ BanglaKhabarWeb/Controllers/SecurityController.cs
@@ -11,14 +11,21 @@ using System.Web.Mvc;
 
 namespace BanglaKhabarWeb.Controllers
 {
+ 
     public class SecurityController : Controller
     {
         ApiRequest apiRequest = new ApiRequest();
-        // GET: Security
+
         public ActionResult Index()
         {
             return RedirectToAction("Index", "Home");
         }
+
+        public ActionResult Login() 
+        {
+            return View();
+        }
+
 
         public ActionResult UserLogin(string EmailPhone, string Password)
         {
@@ -91,12 +98,87 @@ namespace BanglaKhabarWeb.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+        public ActionResult UserSignUp(string FirstName, string LastName, string Phone, string Email, string Password, string RePassword, string Gender, string BirthDate)
+        {
+            try
+            {
+                if(Password!=RePassword)
+                {
+                    TempData["msgAlert"] = "N";
+                    TempData["msgAlertDetails"] = "Password did not matched";
+                    Session["SessionUserId"] = null;
+                    Session["SessionUserEmail"] = null;
+                    Session["SessionUserFirstName"] = null;
+                    Session["SessionUserFullName"] = null;
+                    Session["SessionUserType"] = null;
+                    return RedirectToAction("Index", "Home");
+                }
+                SignUpEntity signUpEntity = new SignUpEntity();
+                signUpEntity.FirstName = FirstName;
+                signUpEntity.LastName = LastName;
+                signUpEntity.Phone = Phone;
+                signUpEntity.Email = Email;
+                signUpEntity.Password = Password;
+                signUpEntity.Gender = Gender;
+                signUpEntity.BirthDate = BirthDate;
+                signUpEntity.NickName = "";
+                signUpEntity.UserId = "0";
+                signUpEntity.UserType = "customer";
+                signUpEntity.UserStatus = "Y";
+
+                var res = apiRequest.HttpPostRequest(signUpEntity, "api/Security/UserSignUp");
+                // response = response.Replace("null", "0"); 
+                string response = res.ToString();
+                var responseModel = JsonConvert.DeserializeObject<ResponseMessage>(response);
+
+                if (responseModel.MessageCode == "Y")
+                {
+                    var userInfo = JsonConvert.DeserializeObject<UserInfo>(responseModel.Content.ToString());
+
+                   
+                        TempData["msgAlert"] = "Y";
+                        TempData["msgAlertDetails"] = responseModel.Message.ToString();
+              
+                        return RedirectToAction("Index", "Home");
+                    
+                }
+
+
+                else
+                {
+                    TempData["msgAlert"] = "N";
+                    TempData["msgAlertDetails"] = responseModel.Message.ToString();
+                    Session["SessionUserId"] = null;
+                    Session["SessionUserEmail"] = null;
+                    Session["SessionUserFirstName"] = null;
+                    Session["SessionUserFullName"] = null;
+                    Session["SessionUserType"] = null;
+                    return RedirectToAction("Index", "Home");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["msgAlert"] = "N";
+                TempData["msgAlertDetails"] = "Sorry, something wrong. Please wait and try after a few minutes.";
+                Session["SessionUserId"] = null;
+                Session["SessionUserEmail"] = null;
+                Session["SessionUserFirstName"] = null;
+                Session["SessionUserFullName"] = null;
+                Session["SessionUserType"] = null;
+                return RedirectToAction("Index", "Home");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
         public ActionResult Logout()
         {
             // Session["SessionUserEmail"] = null;
             Session.Contents.RemoveAll();
             return RedirectToAction("Index", "Home");
         }
+
+
 
     }
 }
